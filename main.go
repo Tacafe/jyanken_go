a17e7609043e5779596ac4d268f032ba4aed4e7f
tacafe <kenjitacafe@gmail.com>
2022-09-23 17:24:47 +0900

    [add] aiko match

--- main.go
+++ main.go
@@ -7,10 +7,17 @@ import (
 )
 
 func main() {
-	initialMatch()
+	var winner string
+	winner = initialMatch()
+
+	for winner == "" {
+		winner = aikoMatch()
+	}
+
+	fmt.Printf("%sの勝ちです。\n", winner)
 }
 
-func initialMatch() {
+func initialMatch() string {
 	fmt.Printf("\nさいしょは\t✊\n")
 	player_hand := jyanken.GetPlayerHand()
 	com_hand := jyanken.GetComputerHand()
@@ -18,8 +25,50 @@ func initialMatch() {
 	fmt.Printf("じゃーんけーん....ポンっ!!!\n")
 	fmt.Printf("YOU:\t%s\n", player_hand)
 	fmt.Printf("COM:\t%s\n\n", com_hand)
+	return getResult(player_hand, com_hand)
 }
 
-	fmt.Printf("さいしょは✊\n")
-	fmt.Printf("じゃーんけーん: %s\n", player_hand)
+func aikoMatch() string {
+	fmt.Printf("あーいこーで....\n")
+	player_hand := jyanken.GetPlayerHand()
+	com_hand := jyanken.GetComputerHand()
+	fmt.Printf("...しょっ!!!\n")
+	fmt.Printf("YOU:\t%s\n", player_hand)
+	fmt.Printf("COM:\t%s\n\n", com_hand)
+	return getResult(player_hand, com_hand)
+}
+
+func getResult(player_hand string, com_hand string) string {
+	switch player_hand {
+	case "グー":
+		switch com_hand {
+		case "グー":
+			return ""
+		case "チョキ":
+			return "YOU"
+		case "パー":
+			return "COM"
+		}
+
+	case "チョキ":
+		switch com_hand {
+		case "グー":
+			return "COM"
+		case "チョキ":
+			return ""
+		case "パー":
+			return "YOU"
+		}
+
+	case "パー":
+		switch com_hand {
+		case "グー":
+			return "YOU"
+		case "チョキ":
+			return "COM"
+		case "パー":
+			return ""
+		}
+	}
+	return ""
 }
